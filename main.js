6e0fc0449e42fc42da9080113d8c2b1cceaaf0af
“xs” <i@caisong.com>
2017-08-05 10:46:23 +0800

    还在写

--- main.js
+++ main.js
@@ -4,17 +4,9 @@ const ws = require('ws')
 const compose = require( 'koa-compose' )
 const WebSocket = ws.Server
 
-class Socket {
-	/**
-	 * OK
-	 * 
-	 * @param  socket     [description]
-	 * @param  listeners  [description]
-	 * @param  middleware [description]
-	 * @param   clientId   [description]
-	 */
-	constructor(socket, listeners, middleware, clientId){
-		this.socket = socket
+class Sock {
+	constructor(sock, listeners, middleware, clientId){
+		this.sock = sock
 		this.middleware = null
 		this.clientId = clientId
 
@@ -22,27 +14,35 @@ class Socket {
 	}
 
 	on(event, handler){
-		let that = this
-		this.socket.on(event, (data, cb) => {
-			let packet = {
-				event: event,
-				data: data,
-				socket: that,
-				acknowledge: cb
-			}
-			if(!that.middleware) {
-				handler(packet, data)
-				return
-			}
+		let packet
+		var that = this
+		this.sock.on('message', (data, cb) => {
+			try{
+				data = JSON.parse(data)
+				if (event != data.cmd){
+					return
+				}
+				data.data = data.data || ''
+				packet = {
+					event: data.cmd,
+					data: data.data,
+					sock: that,
+					acknowledge: cb  // next
+				}
 
-			that.middleware( packet ).then( () => {
-				handler( packet, data )
-			})
+				if ( !this.middleware ) {
+					handler( packet, data )
+					return
+				}
+
+				this.middleware( packet ).then( () => {
+					handler( packet, data )
+				})
+			}catch(e){}
 		})
 	}
 
 	update( listeners, middleware ){
-		// this.socket.removeAllListeners()
 		this.middleware = middleware
 
 		listeners.forEach( (handler, event) => {
@@ -60,13 +60,19 @@ class Socket {
 		return this.clientId
 	}
 
-	send(msg){
-		console.log(msg, 'send')
-		this.socket.send(msg)
+	send(pkg){
+		switch(typeof pkg){
+			case "object":
+				pkg = JSON.stringify(pkg)
+				break;
+			case "string":
+				break;
+		}
+		this.sock.send(pkg)
 	}
 
 	disconnect(){
-		this.socket.close()
+		this.sock.close()
 	}
 }
 
@@ -83,10 +89,9 @@ module.exports = class IO{
 
 		const interval = setInterval(()=>{
 			that.connections.forEach(connection => {
-				if(connection.socket.isAlive == false) return connection.socket.terminate()
-				connection.socket.isAlive = false;
-			console.log('send ping')
-				connection.socket.ping('', false, true)
+				if(connection.sock.isAlive == false) return connection.sock.terminate()
+				connection.sock.isAlive = false;
+				connection.sock.ping('', false, true)
 			})
 		}, 30000)
 	}
@@ -104,8 +109,8 @@ module.exports = class IO{
 
 		app._io = new WebSocket({server: app.server})
 		app.io = this
-		this.socket = app._io
-		this.socket.on('connection', this.onConnect.bind(this))
+		this.websock = app._io
+		this.websock.on('connection', this.onConnect.bind(this))
 	}
 	// it's ok
 	use(fn) {
@@ -114,6 +119,7 @@ module.exports = class IO{
 		this.updateConnections()
 		return this
 	}
+
 	on( event, handler) {
 		let listeners = this.listeners.get(event)
 
@@ -128,34 +134,34 @@ module.exports = class IO{
 		this.updateConnections()
 		return this
 	}
-	broadcast( event, data){ // it's ok
-		this.connections.forEach( (socket) => {
-			socket.send(data);
+	broadcast( cmd, data){ // it's ok
+		this.connections.forEach( (instance) => {
+			try{
+				instance.send({ cmd: cmd, data: data })
+			}catch(e){}
 		})
 	}
-	onConnect(socket){
+	onConnect(sock){
 		let that = this;
-		let instance = new Socket(socket, this.listeners, this.composed, this.cliendId)
-		instance.socket.isAlive = true
+		let instance = new Sock(sock, this.listeners, this.composed, this.cliendId)
+		instance.sock.isAlive = true
 		this.connections.set(this.cliendId, instance)
 		// 断开连接
-		socket.on('close', () => {
+		sock.on('close', () => {
 			this.onDisconnect(this.cliendId)
 		})
 
-		socket.on('pong', ()=>{
-			console.log('pong')
-			instance.socket.isAlive = true;
+		sock.on('pong', ()=>{
+			instance.sock.isAlive = true;
 		})
 
 		let handlers = this.listeners.get('connection')
 		if(handlers){
 			handlers.forEach( handler => {
-				instance.socket.send('welcome men')
 				handler({
 					event: 'connection',
 					data: instance,
-					socket: instance.socket
+					sock: instance.sock
 				}, instance.id)
 			})
 		}
